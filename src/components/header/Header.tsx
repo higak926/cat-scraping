e0d8361a81bac3e7bb1e9f2d5dd7d0ccdbcf91ab
higak926 <42287240+higak926@users.noreply.github.com>
2025-08-26 00:06:48 +0900

    feat: cat-scraping

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./Header.css";
+
+function Header() {
+  const [count, setCount] = useState(0);
+  const beforeStyles = [
+    "whitespace-nowrap",
+    "rounded",
+    "bg-black",
+    "px-2",
+    "py-1",
+    "text-white",
+    "absolute",
+    "-top-12",
+    "left-1/2",
+    "-translate-x-1/2",
+    "before:content-['']",
+    "before:absolute",
+    "before:-translate-x-1/2",
+    "before:left-1/2",
+    "before:top-full",
+    "before:border-4",
+    "before:border-transparent",
+    "before:border-t-black",
+  ].join(" ");
+
+  return (
+    <>
+      <div className="p-3 flex items-center">
+        <a href="/">
+          <img
+            src="src/assets/cat_beer.png"
+            className="w-24 rounded-full"
+          ></img>
+        </a>
+        <div
+          className="ml-3 p-2 border border-cyan-500 tooltip-arrow"
+          onClick={() => setCount(count + 1)}
+        >
+          <span>猫の猫による猫のための</span>
+          <br />
+          <span>スクレイピングサイトだにゃー</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
